a88282f0ed7d9ffe9b5de1e0c317b21cfa45491a
muOb <mutasem.obeidat@sajilni.com>
2025-09-30 01:11:49 +0300

    delete stringify

--- models/User.js
+++ models/User.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    _id: { type: stringify, required: true },
-    name: { type: stringify, required: true },
-    email: { type: string, required: true, unique: true },
-    imageUrl: { type: string, reuired: true },
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
     cartItems: { type: Object, default: {} },
   },
   { minimize: false }
 );
-const User = mongoose.models.user || mongoose.model("user", userSchema); //if there a user in db ,if not create a new one
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
 export default User;
